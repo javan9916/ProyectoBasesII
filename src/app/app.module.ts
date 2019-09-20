02ceb4ae9d2b6deebb9490f73628afdf61b47468
Roberto <robertjr07.rr@gmail.com>
2019-09-20 07:18:03 -0600

    arreglos de bugs luego de hacer merge

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -6,6 +6,7 @@ import { FormsModule, ReactiveFormsModule } from '@angular/forms';
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
+
 import { MonitorComponent } from './monitor/monitor.component';
 import { LoginComponent } from './login/login.component';
 
