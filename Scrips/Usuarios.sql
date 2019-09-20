ac056b38a60d9427dd4eb33ef6b991f5d306ea07
Roberto <robertjr07.rr@gmail.com>
2019-09-20 08:12:11 -0600

    proc de login

--- Scrips/Usuarios.sql
+++ Scrips/Usuarios.sql
@@ -13,16 +13,36 @@ CREATE TABLE Usuarios
 
 SELECT * FROM Usuarios;
 
----DROP PROCEDURE Agregar_Usuario
-
-CREATE PROCEDURE Agregar_Usuario
-@usuario VARCHAR(50), @contraseña VARCHAR(20), @direccion_ip VARCHAR(15), @puerto INT
-AS
-BEGIN
-	INSERT INTO Usuarios(usuario, contraseña, direccion_ip, puerto)
-	VALUES (@usuario, @contraseña, @direccion_ip, @puerto)
-END
-
-EXECUTE Agregar_Usuario @usuario = 'roberto', @contraseña = '123', @direccion_ip = '126.123.64.7', @puerto = 500
-
+---DROP PROC Login_remoto
 go
+CREATE PROC Login_remoto
+@usuario AS VARCHAR(50),
+@contraseña AS VARCHAR(20),
+@direccion_ip AS VARCHAR(15),
+@servidor AS VARCHAR(15),
+@puerto AS NUMERIC,
+@Resultado AS BIT OUTPUT
+AS
+BEGIN TRY
+	exec sp_addlinkedserver
+		@server = @direccion_ip,
+		@srvproduct = @servidor;
+	
+	exec sp_addlinkedsrvlogin
+		@rmtsrvname = @direccion_ip,
+		@rmtuser = N@usuario,
+		@emtpassword = @contraseña
+	
+	exec sp_serveroption
+		@server = @direccion_ip,
+		@optname = 'data access',
+		@optvalue = 'true'
+END TRY
+
+BEGIN CATCH
+	SET @Resultado = 0;
+END CATCH
+go
+--EXECUTE Login_remoto 'roberto', '123', '126.123.64.7', 500, @Resultado
+
+	
